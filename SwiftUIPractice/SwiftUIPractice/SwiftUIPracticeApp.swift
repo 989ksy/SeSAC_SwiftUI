818f74c0965904abe55caeda34657c74fd726ea7
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-14 19:46:29 +0900

    Clone to GithubDesk

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Seungyeon Kim on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
